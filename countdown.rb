c37b1d3714e5f83a485ffb21ce8b3feae27db362
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-25 03:16:34 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,11 @@
-#write your code here
+#write your code here 
+#define the countdown with x seconds 
+#while the countdown will only be going until x is no longer greater than 0 
+#puts im outputting the number of seconds 
+#sleep is the 1 integer argument for the countdown 
+#x -= 1 is stating the countdown is only until 1 
+#end and then a return for HAPPY NEW YEAR! b/c ruby returns the very last line of code that it executes
+#repeat for sleep 
 
 def countdown(x)
   while x > 0 
